66478665209ec61ee9a7610ddc23d7861afebc4c
Agus-IG <agusig026@gmail.com>
2023-04-29 15:07:02 -0300

    creacion de archivos contact, carousel y products

---
+++ src/pages/Productos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Productos = () => (
+    <section className="Productos">
+        <h3>Hello Productos</h3>
+    </section>
+)
+
+export default Productos;
---
+++ src/pages/contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contacts = () => (
+    <section className="Contacts">
+        <h3>Hello Contacts</h3>
+    </section>
+)
+
+export default Contacts;
