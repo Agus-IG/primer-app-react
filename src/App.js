fd5554edd4622b1ac17f03c898c4a3201490e1e4
Agus-IG <agusig026@gmail.com>
2023-04-29 00:15:56 -0300

    implementando About y pageNotFound

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import './App.css';
 import {
+  BrowserRouter,
   Routes,
-  Route
+  Route,
+  Link
 } from "react-router-dom";
 
 import Home from './pages/Home';
+import About from "./pages/About";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/pagenotfound">
+              PageNotFound
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <Routes>
-        
-        <Route path="/" element={<Home />} />
-        
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/pagenotfound' element={<PageNotFound />} />
       </Routes>
-    </div>
+
+    </BrowserRouter>
   );
 }
 
