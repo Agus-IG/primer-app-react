1807b612476f56daed40e4f53414479e970f52bd
Agus-IG <agusig026@gmail.com>
2023-04-30 15:51:17 -0300

    creacion del carousel e importacion a Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 // Home.js
 import React from 'react'
 import './style.css'
+import Carousel from "../components/Carrousel";
+import Imagen from "../img/wallpaperflare.com_wallpaper (1).jpg";
+import Imagen2 from "../img/wallpaperflare.com_wallpaper (2).jpg";
+import Imagen3 from "../img/wallpaperflare.com_wallpaper.jpg";
 
 const Home = () => (
     <section className="Home">
-        <h3>Hello Home</h3>
+        <Carousel imagen={Imagen} imagen2={Imagen2} imagen3={Imagen3} />
     </section>
 )
 
